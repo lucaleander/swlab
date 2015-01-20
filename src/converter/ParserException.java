9a9391761e23911dc5a9a21de74b69b230439805
Kanseki <jon.ru92@gmail.com>
2015-01-20 21:14:55 +0100

    ParserException

---
+++ src/converter/ParserException.java
@@ -0,0 +1,9 @@
+package converter;
+
+public class ParserException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ParserException(String message) {
+		super(message);
+	}
+}
