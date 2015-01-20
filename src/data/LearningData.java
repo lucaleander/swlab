0e6232e9bcc1925ed911ff0bff6ad76f6a972bf1
Kanseki <jon.ru92@gmail.com>
2015-01-20 20:30:53 +0100

    added addExample() and changed examples type to arraylist.

--- src/data/LearningData.java
+++ src/data/LearningData.java
@@ -1,18 +1,24 @@
 package data;
 
+import java.util.ArrayList;
+
 public class LearningData {
 	private Schema schema;
-	private Example[] examples;
+	private ArrayList<Example> examples;
 	
 	
 	public LearningData(Schema schema, IntTargetValue[] intTargetValues, ImageValue[] imageValues) {
 		this.schema = schema;
 		for(int i = 0; i < intTargetValues.length; i++) {
-			this.examples[i] = new Example(intTargetValues[i], imageValues[i]);	
+			this.examples.add(new Example(intTargetValues[i], imageValues[i]));	
 		}		
 	}
 	
-	public Example[] getExamples() {
+	public void addExample(Example example) {
+		this.examples.add(example);
+	}
+	
+	public ArrayList<Example> getExamples() {
 		return this.examples;
 	}
 }
