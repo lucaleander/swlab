33556dc2b9ca1659915b1d92807221093a89e8cc
Kanseki <jon.ru92@gmail.com>
2015-01-21 14:41:22 +0100

    getClassesByCount

--- src/data/Example.java
+++ src/data/Example.java
@@ -1,5 +1,7 @@
 package data;
 
+import java.util.ArrayList;
+
 public class Example {
 	private IntTargetValue intTargetValue;
 	private ImageValue imageValue;
@@ -13,11 +15,28 @@ public class Example {
 		this.imageValue = imageValue;
 	}
 	
+	public IntTargetDefinition getIntTargetDefinition() {
+		return this.intTargetValue.getIntTargetDefinition();
+	}
+	
 	public int getTargetValue() {
 		return this.intTargetValue.getValue();
 	}
 	
 	public ImageValue getImageValue() {
 		return this.imageValue;
+	}
+	
+	public static int[] getClassesByCount(ArrayList<Example> examples) {
+		if(examples.size() == 0) {
+			return null;
+		}
+		
+		int[] classes = new int[examples.get(0).getIntTargetDefinition().getClasses()];
+		for(Example example:examples) {
+			classes[example.getTargetValue()] += 1;
+		}
+		
+		return classes;
 	}
 }
