c9f00b88d63ecfbc68541c6c756c434b239f5da0
Jesper <jesper@gmx.de>
2015-01-22 03:04:45 +0100

    asdfasdfasdfasdf

--- src/gui/localsettings.java
+++ src/gui/localsettings.java
@@ -4,6 +4,8 @@ package gui;
  * Created by luca on 21.01.15.
  */
 
+//Set the default Path to access the train data easier
+
 public class localsettings {
     static final String path_images = "./data/train-images.idx3-ubyte";
     static final String path_labels = "./data/train-labels.idx1-ubyte";
